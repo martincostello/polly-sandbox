9e842ea4e04203a09c21f070ffe3778e0892b88c
martincostello <martin@martincostello.com>
2023-07-23 16:48:04 +0100

    Add benchmarks
    Add a benchmark project for the application using stub HTTP requests.

---
+++ perf/PollySandbox.Benchmarks/AppBenchmarks.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Martin Costello, 2023. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Net.Http.Json;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+
+namespace PollySandbox.Benchmarks;
+
+[EventPipeProfiler(EventPipeProfile.CpuSampling)]
+[MemoryDiagnoser]
+public class AppBenchmarks
+{
+    private AppServer _service;
+    private HttpClient _client;
+
+    [GlobalSetup]
+    public void StartServer()
+    {
+        _service = new AppServer();
+        _client = _service.CreateDefaultClient();
+    }
+
+    [GlobalCleanup]
+    public async Task StopServer()
+        => await _service.DisposeAsync();
+
+    [Benchmark]
+    public async Task<Movie[]> GetMovies()
+        => await _client.GetFromJsonAsync<Movie[]>("/movies");
+
+    [Benchmark]
+    public async Task<Movie> GetMovie()
+        => await _client.GetFromJsonAsync<Movie>("/movies/1");
+
+    [Benchmark]
+    public async Task<User[]> GetUsers()
+        => await _client.GetFromJsonAsync<User[]>("/users");
+
+    [Benchmark]
+    public async Task<User> GetUser()
+        => await _client.GetFromJsonAsync<User>("/users/1");
+}
---
+++ perf/PollySandbox.Benchmarks/AppServer.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Martin Costello, 2023. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Reflection;
+using JustEat.HttpClientInterception;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace PollySandbox;
+
+public class AppServer : WebApplicationFactory<Program>
+{
+    private readonly HttpClientInterceptorOptions _interceptor = new();
+
+    public AppServer()
+    {
+        ClientOptions.AllowAutoRedirect = false;
+        ClientOptions.BaseAddress = new("https://localhost");
+
+        _interceptor.ThrowsOnMissingRegistration()
+                    .RegisterBundle(typeof(AppServer).Assembly.GetCustomAttributes<AssemblyMetadataAttribute>().First(p => p.Key is "HttpBundlePath").Value);
+    }
+
+    public virtual Uri ServerUri => ClientOptions.BaseAddress;
+
+    public virtual HttpClient CreateHttpClientForApp()
+        => CreateDefaultClient();
+
+    protected override void ConfigureClient(HttpClient client)
+    {
+        client.BaseAddress = ServerUri;
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration(builder =>
+               {
+                   builder.AddInMemoryCollection(new[]
+                   {
+                       KeyValuePair.Create("Api:Endpoints:Movies:RateLimit", "1000000"),
+                       KeyValuePair.Create("Api:Endpoints:Users:RateLimit", "1000000"),
+                   });
+               })
+               .ConfigureLogging(builder => builder.ClearProviders())
+               .ConfigureServices(services => services.AddSingleton<IHttpMessageHandlerBuilderFilter, HttpClientInterceptionFilter>((_) => new HttpClientInterceptionFilter(_interceptor)))
+               .UseEnvironment(Environments.Production);
+    }
+
+    private sealed class HttpClientInterceptionFilter : IHttpMessageHandlerBuilderFilter
+    {
+        private readonly HttpClientInterceptorOptions _options;
+
+        public HttpClientInterceptionFilter(HttpClientInterceptorOptions options)
+        {
+            _options = options;
+        }
+
+        /// <inheritdoc/>
+        public Action<HttpMessageHandlerBuilder> Configure(Action<HttpMessageHandlerBuilder> next)
+        {
+            return (builder) =>
+            {
+                next(builder);
+                builder.AdditionalHandlers.Add(_options.CreateHttpMessageHandler());
+            };
+        }
+    }
+}
